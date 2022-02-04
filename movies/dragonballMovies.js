bb8096f64da085d67ac9b045046a79b5fe8d293f
Kai-ulrich <kaijulrich@gmail.com>
2022-02-03 17:08:02 -0800

    commit on 'master'

---
+++ movies/dragonballMovies.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    $('#slideshow .slick').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 3000,
+    });
+  });
+  
+  $(document).ready(() => {
+    $('#slideshow2 .slick2').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 3000,
+    });
+  });
+  
